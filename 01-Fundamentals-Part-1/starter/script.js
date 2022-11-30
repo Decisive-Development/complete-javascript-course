47bfd23cbf25f7feb9acd73753bea4475d19a3cf
Decisive Development <decisive.development1@gmail.com>
2022-11-30 10:53:44 +0000

    stash for save

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,116 @@
+
+// let country = "England";
+// let continent = "Europe";
+// let population = "50000";
+
+// console.log(country, continent, population);
+
+// const markMass = 78;
+// const johnMass = 92;
+// const markHeight = 1.69;
+// const johnHeight = 1.95;
+
+// const bmiMark = markMass / (markHeight * markHeight);
+// const bmiJohn = johnMass / (johnHeight * johnHeight);
+// console.log(bmiMark);
+// console.log(bmiJohn);
+
+// markHigherBMI = bmiMark > bmiJohn;
+// console.log(markHigherBMI);
+
+// const markMass = 95;
+// const markHeight = 1.88;
+// const johnMass = 85;
+// const johnHeight = 1.76;
+
+// const bmiMark = markMass / markHeight ** 2;
+// const bmiJohn = johnMass / johnHeight ** 2;
+
+
+// markHigherBMI = bmiMark > bmiJohn;
+// console.log(bmiMark, bmiJohn, markHigherBMI);
+
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+
+/*
+const massMark = 95;
+const heightMark = 1.88;
+const massJohn = 85;
+const heightJohn = 1.76;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+const markHigherBMI = BMIMark > BMIJohn;
+
+console.log(BMIMark, BMIJohn, markHigherBMI);*/
+
+// Strings and Template Literals
+// const firstName = 'Jonas';
+// const job = 'teacher';
+// const birthYear = 1991;
+// const year = 2037;
+
+// const jonas = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job + '!';
+// console.log(jonas);
+
+// const jonasNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+// console.log(jonasNew);
+
+// console.log(`Just a regular string'in' test`)
+
+// const age = 19;
+// const isOldEnough = age >= 18;
+
+// if(isOldEnough){
+//     console.log('Sarah can start 🐱‍👓');
+// }
+
+// const age = 15
+
+// if(age >= 18){
+//     console.log('Sarah can start 🐱‍👓');
+// } else {
+//     const yearsLeft = 18 - age;
+//     console.log(`Sarah is too young, wait ${yearsLeft} years. `);
+// }
+
+// const birthYear = 2012  ;
+
+// let century;
+// if (birthYear <= 2000) {
+//     century = 20;
+// } else {
+//     century = 21;
+// }
+
+// console.log(century);
+
+
+
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+
+
+// const BMIMark = massMark / heightMark ** 2;
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+
+
+// if (BMIMark > BMIJohn) {
+//     console.log(`Mark's (${BMIMark}) BMI is higher than John's (${BMIJohn})!`);
+// } else {    
+//     console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
+// }
+
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Jonas'));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
